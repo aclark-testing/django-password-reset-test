8951be365ef7395cbf2b1fe0aa936d116f56a707
Alex Clark <aclark@aclark.net>
2014-10-11 18:49:56 -0400

    Update

--- django_password_reset_test/django_password_reset_test/django_password_reset_test/urls.py
+++ django_password_reset_test/django_password_reset_test/django_password_reset_test/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
-from django_password_reset_test import views
+from django_password_reset_test.django_password_reset_test import views
 
 
 urlpatterns = patterns(
